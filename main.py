d057cdf12af7a5ee728da495e566d96f4c0a57fb
HirushaR <hirusharandunu11@gmail.com>
2019-11-04 20:27:12 +0530

    save

--- main.py
+++ main.py
@@ -6,4 +6,11 @@ filename = None
 def newFile():
     global filename
     filename = "Untitled"
-    text.delete(0,0,END)
+    text.delete(0.0,END)#line 0 coloum 0
+
+def saveFile():
+    global filename
+    t = text.get(0.0,END)#store all the text from text box
+    f= open(filename,'w')#open the file
+    f.write(t)#store 
+    f.close()
